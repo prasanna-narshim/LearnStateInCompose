a593e3cceee387afbebb55207b89c55ab2d2cafb
prasanna vishnuram <prasanna.vishnuram@scotiabank.com>
2023-01-08 01:48:17 -0500

    - Add stateless and stateful composables - Add Readme

--- app/src/main/java/com/prasanna/learnstateincompose/MainActivity.kt
+++ app/src/main/java/com/prasanna/learnstateincompose/MainActivity.kt
@@ -3,11 +3,17 @@ package com.prasanna.learnstateincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.prasanna.learnstateincompose.ui.theme.LearnStateInComposeTheme
@@ -19,7 +25,8 @@ class MainActivity : ComponentActivity() {
 			LearnStateInComposeTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-					Greeting("Android")
+					StatelessCompsables()
+					StatefulComposables()
 				}
 			}
 		}
@@ -27,14 +34,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun StatelessCompsables() {
+	var count = 0
+	Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
+		Text(text = "$count times recomposed :( ")
+		Button(onClick = { count++ }) {
+			Text(text = "Tap me!, I am a Stateless Composable")
+		}
+	}
+}
+
+@Composable
+fun StatefulComposables() {
+	val count = remember {
+		mutableStateOf(0)
+	}
+	Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom) {
+		Text(text = "${count.value} times recomposed :) ")
+		Button(onClick = { count.value++ }) {
+			Text(text = "Tap me!, I am a Stateful Composable" )
+		}
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	LearnStateInComposeTheme {
-		Greeting("Android")
+		StatelessCompsables()
+		StatefulComposables()
 	}
 }
